0cb6cc06e238d6e7a3f73f7ef4175287dc04c9a9
Adarshswagh <waghmareadarsh1432@gmail.com>
2026-01-05 17:30:00 +0530

    add

--- app/Http/Controllers/vendor/Chatify/Api/MessagesController.php
+++ app/Http/Controllers/vendor/Chatify/Api/MessagesController.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
+// this is messagecontroller
 
 class MessagesController extends Controller
 {
